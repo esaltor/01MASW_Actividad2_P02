af5007b9dfbb529bd79aa179e35c9ecf123a085d
Ester Alegre <esteralegretorres@gmail.com>
2026-02-22 09:32:19 +0100

    Actualizar seeder con usuarios de prueba

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Rol;
+use App\Models\Usuario;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -23,6 +24,32 @@ class DatabaseSeeder extends Seeder
         Rol::create([
             'nombre' => 'Administrador',
             'descripcion' => 'Rol con privilegios',
+        ]);
+
+        Rol::create([
+            'nombre' => 'Mia',
+            'descripcion' => 'Responsable de Medios Informáticos y Audiovisuales',
+        ]);
+
+        Usuario::create([
+            'nombre' => 'Admin',
+            'email' => 'admin@demo.com',
+            'password' => bcrypt('admin123'),
+            'idRol' => 2, // Asignar el rol de Administrador
+        ]);
+
+        Usuario::create([
+            'nombre' => 'Usuario',
+            'email' => 'usuario@demo.com',
+            'password' => bcrypt('usuario123'),
+            'idRol' => 1, // Asignar el rol de Usuario
+        ]);
+
+        Usuario::create([
+            'nombre' => 'Mia',
+            'email' => 'mia@demo.com',
+            'password' => bcrypt('mia123'),
+            'idRol' => 3, // Asignar el rol de Mia
         ]);
     }
 }
